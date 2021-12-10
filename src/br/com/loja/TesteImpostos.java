3cd01044d32e14e090a9ce84791456997e3991c1
Lucas <lucasmarcos343@gmail.com>
2021-12-10 09:45:28 -0400

    Aplicando o Chain of Responsibility

--- src/br/com/loja/TesteImpostos.java
+++ src/br/com/loja/TesteImpostos.java
@@ -6,10 +6,10 @@ import br.com.loja.imposto.CalculadoraDeImpostos;
 import br.com.loja.imposto.ICMS;
 import br.com.loja.orcamento.Orcamento;
 
-public class TestesImpostos {
+public class TesteImpostos {
 
 	public static void main(String[] args) {
-		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
 		System.out.println(calculadora.calcular(orcamento, new ICMS()));
 	}
--- src/br/com/loja/orcamento/Orcamento.java
+++ src/br/com/loja/orcamento/Orcamento.java
@@ -5,15 +5,19 @@ import java.math.BigDecimal;
 public class Orcamento {
 	
 	private BigDecimal valor;
-	
-	public Orcamento(BigDecimal valor) {
-		super();
+	private int quantidadeItens;
+
+	public Orcamento(BigDecimal valor, int quantidadeItens) {
 		this.valor = valor;
+		this.quantidadeItens = quantidadeItens;
 	}
 
 	public BigDecimal getValor() {
 		return valor;
 	}
-	
+
+	public int getQuantidadeItens() {
+		return quantidadeItens;
+	}
 
 }
