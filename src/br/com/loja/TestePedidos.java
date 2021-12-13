51a50c3cf515766a48855a07d41e7830ab2798c2
Lucas <lucasmarcos343@gmail.com>
2021-12-13 15:01:07 -0400

    Aplicando Observer

--- src/br/com/loja/TestePedidos.java
+++ src/br/com/loja/TestePedidos.java
@@ -1,19 +1,23 @@
 package br.com.loja;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import br.com.loja.pedido.GeraPedido;
 import br.com.loja.pedido.GeraPedidoHandler;
+import br.com.loja.pedido.acao.EnviarEmailPedido;
+import br.com.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 public class TestePedidos {
 	
 	public static void main(String[] args) {
-		String cliente = args[0];
-		BigDecimal valorOrcamento = new BigDecimal(args[1]);
-		int quantidadeItens = Integer.parseInt(args[2]);
+		String cliente = "Rodrigo";
+		BigDecimal valorOrcamento = new BigDecimal("300");
+		int quantidadeItens = Integer.parseInt("2");
 		
 		GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-		GeraPedidoHandler handler = new GeraPedidoHandler(/*dependencias*/);
+		GeraPedidoHandler handler = new GeraPedidoHandler(Arrays.asList(new SalvarPedidoNoBancoDeDados(), 
+																		new EnviarEmailPedido()));
 		handler.execute(gerador);
 	}
 
